b3e9a1d2393763a481385efd246c7db65ab32d16
Dean Smith <dean@zelotus.com>
2020-01-24 19:16:40 +0000

    First properly working version
    . log function
    . new torque measurements after 'eureka' eddy current moments,
    still need proper method to measure these but removes
    random scale factor
    . cleaned up event code
    . actual times calculated in events

--- BluePower/BluePower.ino
+++ BluePower/BluePower.ino
@@ -1,33 +1,49 @@
 #include <ArduinoBLE.h>
+#include <stdarg.h>
 #define BATTERYLEVEL 72
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int force_levels[] = {600,700,800,900,1100,1300,1500,1900};
-const long crank_length = 0.16;
-uint16_t crank=0;
-  
+const int pedalSensorPin = 2;     // the number of input for the cadence sensor
+const int switchPin = 3;     // the number of input for the power switch
+volatile int powerSetting;
+const float forceLevels[] = {1000,3000,800,900,14000,16000,12000,20000};
+const float crankLength = 0.175f;
+volatile uint16_t crank=0;
+volatile unsigned long lastEvent; // time in ms since boot of most recent revolution
+volatile unsigned long deltaTime; // tims in ms between previous rotation and lastEvent
 
 BLEService powerService("1818");
 BLECharacteristic powerLevelChar("2A63", BLERead | BLENotify, 4, false);
 BLECharacteristic cadenceChar("2A5B", BLERead | BLENotify, 5, false);
-char printBuffer[1024];
-unsigned long lastEvent;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
+  log("Logging %s","test");
+
   pinMode(LED_BUILTIN, OUTPUT);
   if (!BLE.begin())
   {
-    Serial.println("starting BLE failed!");
+    log("starting BLE failed!");
     while (1);
   }
 
-  lastEvent=millis();
+  lastEvent=millis(); // Initialise lastEvent with sane value
 
- attachInterrupt(digitalPinToInterrupt(2), pin_ISR, CHANGE);
+  // Set pins to input mode
+  pinMode(pedalSensorPin,INPUT); 
+  pinMode(switchPin,INPUT);
 
+  // Attach interupts
+  attachInterrupt(digitalPinToInterrupt(pedalSensorPin), isrPedal, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(switchPin), isrPowerSwitch, CHANGE);
 
+  // Power setting
+  if (digitalRead(pedalSensorPin)) {
+    powerSetting = 8;
+  } else {
+    powerSetting = 7;
+  }
+  
   BLE.setDeviceName("BluePower");
   BLE.setLocalName("BluePower");
   BLE.setAdvertisedService(powerService);
@@ -35,21 +51,58 @@ void setup() {
   powerService.addCharacteristic(cadenceChar);
   BLE.addService(powerService);
   BLE.advertise();
-  Serial.println("Bluetooth device active, waiting for connections...");
+  log("Bluetooth device active, waiting for connections...");
 }
 
 
 
-void pin_ISR() { 
+void isrPedal() { 
+    bool newState;
+    bool pedalState;
+    unsigned long thisEvent;
+
+    thisEvent = millis();
+    newState = digitalRead(pedalSensorPin);
+
+    if ((thisEvent - lastEvent > 100) && newState != pedalState) { // 100ms min for a pedal rotation, seems fair
+      crank++;
+      pedalState=newState;
+      deltaTime = thisEvent - lastEvent;
+      lastEvent=thisEvent;     
+    } 
+
+}
+
+void isrPowerSwitch() {
     bool buttonState;
 
-  buttonState = digitalRead(buttonPin);
-  if (buttonState) {
-    Serial.println("Revolution");
-    crank++;
+    // Let it settle, only viable for low change
+    delay(25); 
+    
+    buttonState = digitalRead(switchPin);
+  if (
+  buttonState ) {
+      log("Power 8");
+      powerSetting=8;
+    } else { 
+      log("Power 7");
+      powerSetting=7;
+    }
   }
 
-} 
+
+// https://en.wikibooks.org/wiki/C_Programming/stdarg.h
+void log(const char *format,...) {
+  char printBuffer[1024];
+  va_list args;
+  
+  va_start(args,format);
+  vsprintf(printBuffer, format, args); 
+  Serial.println(printBuffer);
+  va_end(args); 
+}  
+
+
 
 void loop()
 {  
@@ -57,27 +110,33 @@ void loop()
   char powerData[4];
   char cadenceData[5];
   unsigned long thisEvent;
-  uint16_t lastTime=0;
   uint16_t power=0;
   int resistor;
   int old_crank;
+  float rpm;
+  float torque;
+ 
   
   central = BLE.central();
   if (central)
   {
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
+    log("Connected to central: %s", central.address());
     digitalWrite(LED_BUILTIN, HIGH);
 
     while (central.connected()) {
-      power = 100 + rand() % 25 ; // ???? I wonder what that equates to...
+      //power = 100 + rand() % 25 ; // ???? I wonder what that equates to...
 
       if (old_crank != crank) { // There has been a revolution -
       old_crank=crank;
       
-      thisEvent=millis();
-      lastEvent=thisEvent;
-      lastTime = thisEvent & 0xffff;
+      
+      // Calc rpm
+      rpm = (60000 / deltaTime); // one minute in ms divided by time between last two events
+      // Calculate Power
+      // Torque
+      torque = (forceLevels[powerSetting-1] / 1000) * 9.81 * crankLength; 
+      // Power
+      power = (int) ( torque * PI * ( rpm/30 ));
       
       powerData[0]=0; // NO FLAGS
       powerData[1]=0; // NO FLAGS
@@ -88,11 +147,10 @@ void loop()
       cadenceData[0] = 0b10; // Crank only
       cadenceData[1] = crank & 0xff;
       cadenceData[2] = (crank >> 8) & 0xff;
-      cadenceData[3] = lastTime % 256;
-      cadenceData[4] = lastTime / 256;
+      cadenceData[3] = (lastEvent & 0xffff) % 0xff;
+      cadenceData[4] = (lastEvent & 0xffff) / 0xff;
       cadenceChar.writeValue(cadenceData,5);
-      sprintf(printBuffer,"Set power to %d at time %d last time %d crank %d resistor %d\n", power, millis(),lastTime,crank,resistor);
-      Serial.print(printBuffer);
+      log("rpm %f torque %f power %d lastTime %d crank %d", rpm, torque, power, lastEvent, crank);      
       } else {
         // No changes
       }
