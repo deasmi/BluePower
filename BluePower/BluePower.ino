173a12442b7c2145516c300438f851ad807318af
Dean Smith <dean@zelotus.com>
2020-01-31 20:04:45 +0000

    New interupt code for timers

--- BluePower/BluePower.ino
+++ BluePower/BluePower.ino
@@ -166,29 +166,21 @@ void updateDisplay()
 	len=sprintf(buf,"%lu",currentRPM);
 	display.setCursor(82-(6*len),34);
 	display.print(buf);
+
+	if (statusDot==WHITE) {
+		statusDot=BLACK; } else {
+		statusDot=WHITE; }
+	display.fillRect(80,44,2,2,statusDot);
 	
 	display.display();	
 }
-/***********************************
- * Setup and loop below here       *
- ***********************************/
 
-void setup() {
-  Serial.begin(9600);
-  //while (!Serial);
 
-	initDisplay(); // Setup and show logo
-	delay(1000); // Wait 1s before main display
+void setupInterupts() {
 
-	if (!BLE.begin())
-  {
-    log("starting BLE failed!");
-    while (1);
-  }
+	// Input interupts
 
-  lastEvent=millis(); // Initialise lastEvent with sane value
-
-  // Set pins to input mode
+	// Set pins to input mode
   pinMode(pedalSensorPin,INPUT); 
   pinMode(upButton,INPUT);
   pinMode(downButton,INPUT);
@@ -198,6 +190,108 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(downButton), upButtonIH, RISING);
   attachInterrupt(digitalPinToInterrupt(upButton), downButtonIH, RISING);
 
+
+	// 0.25s timer for data and screen updates
+	tcConfigure(updateRate);
+	tcStartCounter();
+}	
+	
+void TC5_Handler(void) {
+	updateValues();
+	updateDisplay();
+	TC5->COUNT16.INTFLAG.bit.MC0 = 1; // Reset interupt
+}
+	
+/* https://gist.github.com/nonsintetic/ad13e70f164801325f5f552f84306d6f */
+/* 
+ *  TIMER SPECIFIC FUNCTIONS FOLLOW
+ *  you shouldn't change these unless you know what you're doing
+ */
+
+//Configures the TC to generate output events at the sample frequency.
+//Configures the TC in Frequency Generation mode, with an event output once
+//each time the audio sample frequency period expires.
+ void tcConfigure(int sampleRate)
+{
+ // Enable GCLK for TCC2 and TC5 (timer counter input clock)
+ GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5)) ;
+ while (GCLK->STATUS.bit.SYNCBUSY);
+
+ tcReset(); //reset TC5
+
+ // Set Timer counter Mode to 16 bits
+ TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+ // Set TC5 mode as match frequency
+ TC5->COUNT16.CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+ //set prescaler and enable TC5
+ TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024 | TC_CTRLA_ENABLE; //you can use different prescaler divisons here like TC_CTRLA_PRESCALER_DIV1 to get different ranges of frequencies
+ //set TC5 timer counter based off of the system clock and the user defined sample rate or waveform
+ TC5->COUNT16.CC[0].reg = (uint16_t) (SystemCoreClock / sampleRate - 1);
+ while (tcIsSyncing());
+ 
+ // Configure interrupt request
+ NVIC_DisableIRQ(TC5_IRQn);
+ NVIC_ClearPendingIRQ(TC5_IRQn);
+ NVIC_SetPriority(TC5_IRQn, 0);
+ NVIC_EnableIRQ(TC5_IRQn);
+
+ // Enable the TC5 interrupt request
+ TC5->COUNT16.INTENSET.bit.MC0 = 1;
+ while (tcIsSyncing()); //wait until TC5 is done syncing 
+} 
+
+//Function that is used to check if TC5 is done syncing
+//returns true when it is done syncing
+bool tcIsSyncing()
+{
+  return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY;
+}
+
+//This function enables TC5 and waits for it to be ready
+void tcStartCounter()
+{
+  TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; //set the CTRLA register
+  while (tcIsSyncing()); //wait until snyc'd
+}
+
+//Reset TC5 
+void tcReset()
+{
+  TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
+  while (tcIsSyncing());
+  while (TC5->COUNT16.CTRLA.bit.SWRST);
+}
+
+//disable TC5
+void tcDisable()
+{
+  TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
+  while (tcIsSyncing());
+}
+
+/***********************************
+ * Setup and loop below here       *
+ ***********************************/
+
+void setup() {
+#ifdef DEBUG
+	// No serial if we aren't debugging
+	Serial.begin(9600);
+#endif
+
+	initDisplay(); // Setup and show logo
+	delay(2000); // Wait 1s before main display
+
+	if (!BLE.begin())
+  {
+    log("starting BLE failed!");
+    while (1);
+  }
+
+  lastEvent=millis(); // Initialise lastEvent with sane value
+
+	setupInterupts();
+	
 	// Setup key variables
   powerSetting=5; // Default to a mid setting
 	currentPower=0; // Assume static
